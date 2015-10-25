42ec57ab80e091caf083838d908e1b28d177ccea
unknown <csudbery@dltd00ym32.ad.laterooms.com>
2015-10-25 06:12:24 +0000

    now adding game Id to gamestate endpoint

--- src/lib/DataAccessor.js
+++ src/lib/DataAccessor.js
@@ -1,9 +1,14 @@
 define('dataAccessor', ['jquery'], function($) {
     return {
         getGameState: function(onSuccess) {
+
+            var gameId = window.location.hash.substring(1, window.location.hash.length);
+            //var gameStateBaseEndpoint = 'http://youdecide.local/gamestate';
+            var gameStateBaseEndpoint = 'http://youdecideapi.apphb.com/gamestate';
+            var gameStateFullEndpoint = gameStateBaseEndpoint + '/' + gameId;
+
             $.ajax({
-                url: 'http://youdecideapi.apphb.com/gamestate',
-                //url: 'http://youdecide.local//gamestate',
+                url: gameStateFullEndpoint,
                 type: 'get',
                 dataType: 'json',
                 success: onSuccess
