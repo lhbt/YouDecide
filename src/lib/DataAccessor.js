6db4ef56c17fdbcea7586f893a17a8982d7c7a40
unknown <csudbery@dltd00ym32.ad.laterooms.com>
2015-10-25 02:25:14 +0000

    More prettifying!

--- src/lib/DataAccessor.js
+++ src/lib/DataAccessor.js
@@ -2,8 +2,8 @@ define('dataAccessor', ['jquery'], function($) {
     return {
         getGameState: function(onSuccess) {
             $.ajax({
-                url: 'http://youdecideapi.apphb.com/gamestate',
-                //url: 'http://youdecide.local//gamestate',
+                //url: 'http://youdecideapi.apphb.com/gamestate',
+                url: 'http://youdecide.local//gamestate',
                 type: 'get',
                 dataType: 'json',
                 success: onSuccess
