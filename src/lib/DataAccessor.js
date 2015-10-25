e7f76b37fb7e26e21a5bebef6cf72a94fbc27881
unknown <csudbery@dltd00ym32.ad.laterooms.com>
2015-10-25 01:52:07 +0000

    Prettied things up a little.

--- src/lib/DataAccessor.js
+++ src/lib/DataAccessor.js
@@ -2,7 +2,8 @@ define('dataAccessor', ['jquery'], function($) {
     return {
         getGameState: function(onSuccess) {
             $.ajax({
-                url: 'http://youdecideapi.apphb.com/gamestate',
+                //url: 'http://youdecideapi.apphb.com/gamestate',
+                url: 'http://youdecide.local//gamestate',
                 type: 'get',
                 dataType: 'json',
                 success: onSuccess
--- src/view-models/GameStateViewModel.js
+++ src/view-models/GameStateViewModel.js
@@ -7,15 +7,23 @@ define('gameStateViewModel', ['dataAccessor'], function (dataAccessor) {
                 state.HasOptions = true;
                 state.History = data.History;
                 state.DeathlyDeathText = data.DeathlyDeathText;
+                state.NextOptions = data.DeathlyDeathText.length > 0 ? ("But it's ok, you can have another go. " + options) : options;
+                state.HasDeathlyDeathText = data.DeathlyDeathText.length > 0 ? true : false;
+                state.HasHistory = data.History.length > 1 ? true : false;
             }
         )
     };
+
+    var options = "Your options now are...";
 
     var state = {
         Options: [],
         HasOptions: false,
         History: "",
-        DeathlyDeathText: ""
+        DeathlyDeathText: "",
+        NextOptions: "",
+        HasDeathlyDeathText: false,
+        HasHistory: false,
     };
     
     var timer = setInterval(getGameState, 1000);
